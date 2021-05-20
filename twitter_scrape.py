39f285ed35d70524ba014a933ed458fb44031213
Abeerhasan1170 <45686825+Abeerhasan1170@users.noreply.github.com>
2021-05-20 12:42:58 +0600

    Update twitter_scrape.py

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -8,7 +8,7 @@ browser = webdriver.Chrome('/home/sifat/scrapping/chromedriver')
 browser.get("https://twitter.com/Sah75official")
 time.sleep(1)
 
-elem = browser.find_element_by_tag_name("body")#grab whole thing....https://stackoverflow.com/questions/21006940/how-to-load-all-entries-in-an-infinite-scroll-at-once-to-parse-the-html-in-pytho
+elem = browser.find_element_by_tag_name("body")#grab the whole thing....https://stackoverflow.com/questions/21006940/how-to-load-all-entries-in-an-infinite-scroll-at-once-to-parse-the-html-in-pytho
 
 no_of_pagedowns = 40
 
@@ -16,21 +16,21 @@ while no_of_pagedowns:
     elem.send_keys(Keys.PAGE_DOWN)
     time.sleep(0.2)
     no_of_pagedowns-=1
-
-post_elems = browser.find_elements_by_class_name("js-stream-item")#class defining..where you want to start scrapping 
-
-page = [(post.get_attribute('innerHTML')) for post in post_elems] #genarte a list of html code from webelement 
+#class defining..where you want to start scrapping 
+post_elems = browser.find_elements_by_class_name("js-stream-item")
+#genarte a list of html code from webelement 
+page = [(post.get_attribute('innerHTML')) for post in post_elems] 
 page = ''.join(page)#convert the list into a string
+ #beautifulsoup object
+soup = BeautifulSoup(page,'html.parser')
 
-soup = BeautifulSoup(page,'html.parser')#beautifulsoup object
+#scrapping the div where class name is tweet.
+div= soup.find_all('div','tweet')
+#scrape attribute value 
+attribute = [i.attrs for i in div]
 
-
-div= soup.find_all('div','tweet')#scrapping the div where class name is tweet.
-
-attribute = [i.attrs for i in div]#scrape attribute value 
-
-
-data = [j['data-permalink-path'] for j in attribute]#from that value choose which you want
+#from that value choose which you want
+data = [j['data-permalink-path'] for j in attribute]
 
 #save that data into a text file
 f = open('log.txt','w')
